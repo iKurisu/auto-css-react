82a3f2d23f7ce077615fec6de4ec5f711dcb2532
iKurisu <chris.epic01@gmail.com>
2019-12-22 18:14:50 +0100

    add main logic

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,49 @@
+#!/usr/bin/env node
 import fs from "fs";
+import commander from "commander";
+import chokidar from "chokidar";
+import { matchExt, fillComponent } from "./utils/file";
 
-console.log(
-  __dirname
-    .split("\\")
-    .slice(0, 5)
-    .concat("src")
-    .join("/")
-);
+const program = new commander.Command();
+program.version("0.0.1");
 
-// REMOVE
-const path = "C:/Users/Usuario/Projects/auto-css-react/src";
+program
+  .option("-e, --extension <ext>", "extension of the stylesheets", "css")
+  .option("-f --autofill", "autofill component")
+  .parse(process.argv);
 
-fs.watch(
-  __dirname
-    .split("\\")
-    .slice(0, 5)
-    .concat("src")
-    .join("/"),
-  (eventType, filename): void => {
-    if (eventType === "rename") {
-      fs.openSync(path + "/A.scss", "w");
-      console.log(eventType, filename);
+const createStylesheet = (path: string): void => {
+  const d = fs.openSync(path, "a");
+  console.log(`Added ${path}`);
+  fs.closeSync(d);
+};
+
+const fillFile = (path: string, stylePath: string): void => {
+  fs.readFile(path, "utf-8", err => {
+    if (err) console.error(err);
+
+    const split = stylePath.split("\\");
+    const fileName = split[split.length - 1];
+
+    fs.writeFile(path, fillComponent(fileName), err => {
+      if (err) console.error(err);
+    });
+  });
+};
+
+chokidar
+  .watch(".", { ignoreInitial: true })
+  .on("ready", () => console.log("Watching for new files..."))
+  .on("all", (event, path) => {
+    const ext = matchExt(path);
+
+    if (event === "add" && (ext === ".tsx" || ext === ".jsx")) {
+      const stylePath = path.replace(ext, `.${program.extension}`);
+
+      createStylesheet(stylePath);
+
+      if (program.autofill) {
+        fillFile(path, stylePath);
+      }
     }
-  }
-);
+  });
